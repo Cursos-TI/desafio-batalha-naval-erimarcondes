fecae0c1949473aecf45278261f6df566b66f8fc
erimarcondes <93197765+erimarcondes@users.noreply.github.com>
2025-08-18 23:32:23 -0300

    Implementação das habilidades no tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -11,6 +11,8 @@ int main(){
     // Declaração da matriz do tabuleiro
     int tabuleiro[10][10] = {0};
 
+    // NAVIOS:
+    
     // Navio horizontal
     int navioH[3] = {3, 3, 3};
     for (int n = 0; n < 3; n++) {
@@ -34,6 +36,87 @@ int main(){
     for (int n = 0; n < 3; n++) {
         tabuleiro[4+n][9-n] = navioD2[n];
     }
+
+    // HABILIDADES:
+
+    // Cone
+    int cone[3][5] = {0};
+    for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+        switch (i) {
+            case 0:
+            if (j == 2) {
+                cone[i][j] = 5;
+            }
+            break;
+
+            case 1:
+            if (j > 0 && j < 4) {
+                cone[i][j] = 5;
+            }
+            break;
+
+            case 2:
+            cone[i][j] = 5;
+            break;
+        }
+        tabuleiro[i][1+j] = cone[i][j];
+    }
+    }
+
+
+    // Cruz
+    int cruz[3][5] = {0};
+    for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+        switch (i) {
+            case 0:
+            if (j == 2) {
+                cruz[i][j] = 5;
+            }
+            break;
+
+            case 1:
+            cruz[i][j] = 5;
+            break;
+
+            case 2:
+            if (j == 2) {
+                cruz[i][j] = 5;
+            }
+            break;
+        }
+        tabuleiro[5+i][2+j] = cruz[i][j];      
+    }
+    }
+
+    // Octaedro
+    int octaedro[3][5] = {0};
+    for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+        switch (i) {
+            case 0:
+            if (j == 2) {
+                octaedro[i][j] = 5;
+            }
+            break;
+
+            case 1:
+            if (j > 0 && j < 4) {
+                octaedro[i][j] = 5;
+            }
+            break;
+
+            case 2:
+            if (j == 2) {
+                octaedro[i][j] = 5;
+            }
+            break;
+        }
+        tabuleiro[7+i][5+j] = octaedro[i][j];
+    }
+    }
+
 
     printf("TABULEIRO - BATALHA NAVAL \n\n\t");
     // Mostra as colunas
